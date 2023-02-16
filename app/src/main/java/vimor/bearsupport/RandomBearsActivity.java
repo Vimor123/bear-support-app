5d462f7746b8e92bd0957df9ba9f3756a380c6a2
Vimor123 <vmladar1@gmail.com>
2023-02-16 14:25:59 +0100

    Done

--- app/src/main/java/vimor/bearsupport/RandomBearsActivity.java
+++ app/src/main/java/vimor/bearsupport/RandomBearsActivity.java
@@ -4,17 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class RandomBearsActivity extends AppCompatActivity {
 
-    public void setNewBear() {
+    private Random rand = new Random();
 
+    public void setNewBear() {
+        int[] bearImageIds = {
+                R.drawable.badonklin,
+                R.drawable.biggiant,
+                R.drawable.cowboy,
+                R.drawable.cuddly,
+                R.drawable.daddy,
+                R.drawable.dreamy,
+                R.drawable.drink,
+                R.drawable.handsome,
+                R.drawable.neonboof1,
+                R.drawable.neonboof2,
+                R.drawable.older,
+                R.drawable.oscar,
+                R.drawable.oscar2,
+                R.drawable.oscarbear,
+                R.drawable.sexy,
+                R.drawable.thiccnd
+        };
+
+        int randomIndex = rand.nextInt(bearImageIds.length);
+
+        ImageView bearImage = (ImageView) findViewById(R.id.bearImageView);
+        bearImage.setImageResource(bearImageIds[randomIndex]);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_bears);
+        setNewBear();
     }
 
     public void newBear(View view) {
